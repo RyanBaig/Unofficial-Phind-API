f9ac35cf0a9ba2953878f67487d685a239c1bdad
RyanBaig <rg5373429@gmail.com>
2024-02-07 15:01:05 +0500

    Phind-API, remastered!

--- main.py
+++ main.py
@@ -1,49 +1,37 @@
-from selenium import webdriver
-from bs4 import BeautifulSoup
-from halo import Halo
 import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 # Initialize vars
 query = input("Input your query: ")
-url = "https://phind.com/agent?q=" + query
+url = "https://www.phind.com/search?q=" + query
 
-# Initialize the webdriver
-driver = webdriver.Chrome()
+# Configure ChromeOptions to suppress logging
+chrome_options = Options()
+chrome_options.add_argument("--log-level=3")  # Fatal errors only
+chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+# Initialize the webdriver with the modified options
+driver = webdriver.Chrome(options=chrome_options)
 
 try:
-    # Start the spinner 
-    spinner = Halo("Getting Answer from Phind...\n\n\n\n\n \n", spinner="dots")
-    spinner.start()
-
     # Get the page content
     driver.get(url)
 
-    # Set a maximum waiting time just in case it doesnt work
-    max_wait_time = 30  
+    # Wait for the DOM to be fully loaded
+    WebDriverWait(driver, timeout=50).until(EC.presence_of_element_located((By.TAG_NAME, "body")))
+    time.sleep(10)
+    # Get the element with the specific CSS selector
+    answer_element = driver.find_element(By.CSS_SELECTOR, "main div.fs-5 p.text-black.mb-2.text-break")
 
-    # Wait for content to load
-    start_time = time.time()
-    while time.time() - start_time < max_wait_time:
-        if driver.execute_script("return document.readyState") == "complete":
-            break
-        time.sleep(1)
+    # Extract text content of the paragraph element
+    desired_text = answer_element.text
 
-    # Get the page source
-    html_content = driver.page_source
-
-    # Stop spinner
-    spinner.stop()
-
-    # Parse the HTML
-    soup = BeautifulSoup(html_content, 'html.parser')
-
-    # Find all paragraphs
-    paragraphs = soup.select('div.fs-5 p.text-black.mb-2.text-break')
-
-    # Print the text content of each paragraph
-    for paragraph in paragraphs:
-        print("\n")
-        print(paragraph.text)
+    # Print the extracted text
+    print("Desired text:", desired_text.strip())
 
 finally:
     # Close the browser
